50d90c4964e3b14f66570c44ad12766b167c89b4
Robert Bounds <113385990+rlb77c@users.noreply.github.com>
2022-09-13 13:27:30 -0400

    Update streamlit_app.py
    Added table for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# put data in table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the table
+streamlit.dataframe(fruityvice_normalized)
